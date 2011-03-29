2a38fad83af1be02839a25c750a29234f32f804e
Per Kraulis <per.kraulis@scilifelab.se>
2011-03-29 16:00:13 +0200

    Removed old subdirectory from git tree.

--- old/designs_instrumentrun/views/name.js
+++
@@ -1,6 +0,0 @@
-/* Index instrumentrun documents by name.
-   Value: null. */
-function(doc) {
-  if (doc.entity !== 'instrumentrun') return;
-  emit(doc.name, null);
-}
--- old/designs_instrumentrun/views/operator.js
+++
@@ -1,6 +0,0 @@
-/* Index instrumentrun documents by operator.
-   Value: null. */
-function(doc) {
-    if (doc.entity !== 'instrumentrun') return;
-    emit(doc.operator, null);
-}
